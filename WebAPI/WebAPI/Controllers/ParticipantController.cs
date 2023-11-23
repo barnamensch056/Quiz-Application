027ac450a00555a22eb6afd75f2f97c2f0792b9c
barnamensch056 <barnadipdey2510@gmail.com>
2023-11-23 12:03:13 +0530

    Quiz app

---
+++ WebAPI/WebAPI/Controllers/ParticipantController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantController : Controller
+    {
+        private readonly QuizDbContext _context;
+
+        public ParticipantController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Participant
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Participant>>> GetParticipants()
+        {
+              return await _context.Participants.ToListAsync();
+        }
+
+        // GET: Participant/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Participant>> GetParticipant(int id)
+        {
+            var participant = await _context.Participants.FindAsync(id);
+
+            if (participant == null)
+            {
+                return NotFound();
+            }
+
+            return participant;
+        }
+
+
+
+        // GET: Participant/Edit/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutParticipant(int id, Participant _participantResult)
+        {
+            if (id != _participantResult.ParticipantId)
+            {
+                return BadRequest();
+            }
+
+            Participant participant=_context.Participants.Find(id);
+            participant.Score=_participantResult.Score;
+            participant.TimeTaken=_participantResult.TimeTaken;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ParticipantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Participant>> PostParticipant(Participant participant)
+        {
+            var temp = _context.Participants
+                .Where(x => x.Name == participant.Name
+                && x.Email == participant.Email
+                ).FirstOrDefault();
+
+            if (temp == null)
+            {
+                _context.Participants.Add(participant);
+                await _context.SaveChangesAsync();
+            }
+            else
+                participant=temp;
+            return Ok(participant);
+        }
+        // GET: Participant/Delete/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteParticipant(int id)
+        {
+            if (id == null || _context.Participants == null)
+            {
+                return NotFound();
+            }
+
+            var participant = await _context.Participants
+                .FirstOrDefaultAsync(m => m.ParticipantId == id);
+            if (participant == null)
+            {
+                return NotFound();
+            }
+
+            return View(participant);
+        }
+
+        private bool ParticipantExists(int id)
+        {
+          return (_context.Participants?.Any(e => e.ParticipantId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : Controller
+    {
+        
+        private readonly QuizDbContext _context;
+
+        public QuestionController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            var random5Qns=await(_context.Questions
+                .Select(x=> new
+                {
+                   QnId=x.QnId,
+                   QnInWords=x.Qn,
+                   ImageName=x.ImageName,
+                   Options= new string[] {x.Option1,x.Option2,x.Option3,x.Option4}
+
+                }).OrderBy(x=>Guid.NewGuid())
+                .Take(6)
+                ).ToListAsync();
+            return Ok(random5Qns);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            return question;
+        }
+        [HttpPost]
+        [Route("GetAnswers")]
+        public async Task<ActionResult<Question>> RetrieveAnswers(int[] qnIds)
+        {
+            var answers = await (_context.Questions
+                 .Where(x => qnIds.Contains(x.QnId))
+                 .Select(y => new
+                 {
+                     QnId = y.QnId,
+                     QnInWords = y.Qn,
+                     ImageName = y.ImageName,
+                     Options = new string[] { y.Option1, y.Option2, y.Option3, y.Option4 },
+                      Answer = y.Answer
+                 })).ToListAsync();
+            return Ok(answers);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestion(int id, Question question)
+        {
+            if (id != question.QnId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult>DeleteQuestion(int id)
+        {
+            var question=await _context.Questions.FindAsync(id);
+            if(question == null)
+            {
+                return NotFound();
+            }
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.QnId == id);
+        }
+
+    }
+}
